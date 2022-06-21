4672c4b5ae28d28c95b9fc1f83190704c81bd0c2
uchihadedasamogon <107944735+uchihadedasamogon@users.noreply.github.com>
2022-06-21 19:34:33 +0300

    Add files via upload

---
+++ угадай число.py
@@ -0,0 +1,13 @@
+import random
+number_random=random.randint(1, 10)
+attempt=0
+while True:
+    number_user=int(input())
+    if number_random == number_user:
+        print("хорош, мега хорош, мега супер пупер хорош")
+        if attempt>3:
+            print("лох")
+    else:
+        print("не хорош")
+        attempt=attempt+1
+        print(attempt, "мне лень писать  твои попытки так что их выведет комп  ")
